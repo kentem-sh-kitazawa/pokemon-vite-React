cc06b108f78090ac4513983cb050d2a0c4f81e8e
sh-kitazawa <sh-kitazawa@kentem.co.jp>
2025-06-04 17:45:10 +0900

    ポケモンの初期表示を実装（リポジトリを作り直したためまとめてコミット）

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import axios from "axios";
+import { useState, useEffect, useContext, createContext } from "react";
+// import PokemonList from './components/pokemonList';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const initialUrl = "https://pokeapi.co/api/v2/pokemon";
+  //最後の数字を変えてあげると個体の情報が取得できる https://pokeapi.co/api/v2/pokemon/1
+
+  //名前とURLをstateで管理
+  const [pokemonUrls, setPokemonUrls] = useState<string[]>([]);
+  //名前データを取得する処理
+  useEffect(() => {
+    const fetchPokemonUrls = async () => {
+      const response = await axios.get(initialUrl);
+      setPokemonUrls(response.data.results.map((result: any) => result.url));
+    };
+    fetchPokemonUrls();
+  }, []);
+
+  //個別のデータをstateで管理
+  const [pokemonData, setPokemonData] = useState<any[]>([]);
+
+  //ページ数をstateで管理
+  const [page, setPage] = useState<number>(0);
+
+  //個別のデータを取得する処理
+  useEffect(() => {
+    const uppdatePokemonData = async () => {
+      const tmpPokemonArray: any[] = [];
+
+      const fetchPokemonData = async () => {
+        for (const url of pokemonUrls) {
+          const pokemonData = await axios.get(url);
+          tmpPokemonArray.push(pokemonData.data);
+        }
+      };
+      await fetchPokemonData();
+      setPokemonData(tmpPokemonArray);
+    };
+
+    uppdatePokemonData();
+  }, [pokemonUrls]);
+
+  //確認用
+  useEffect(() => {
+    console.log(pokemonData);
+  }, [pokemonData]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      {pokemonData.map((pokemon) => (
+        <ul key={pokemon.name}>
+          {/* PokemonListに移動 */}
+          <img src={pokemon.sprites.front_default} alt="" />
+          <h3 key={pokemon.name}>{pokemon.name}</h3>
+          <p>タイプ</p>
+          {pokemon.types.map((type:any)=>(
+            <p>{type.type.name}</p>
+          ))}
+          <p>重さ:{pokemon.weight}</p>
+          <p>高さ:{pokemon.height}</p>
+          {pokemon.abilities.map((ability:any)=>(
+          <p>アビリティ:{ability.ability.name}</p>
+          ))}
+        </ul>
+      ))}
+      {/* 20項目を表示する */}
+      {/* 前へボタン */}
+      <button>前へ</button>
+      {/* 次へボタン */}
+      <button>次へ</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
