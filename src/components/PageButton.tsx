a8e01b4e37f57bbbe55229c904b3b1db0f5f80ce
sh-kitazawa <sh-kitazawa@kentem.co.jp>
2025-06-05 11:41:35 +0900

    PageButton.tsxのスタイル適用

--- src/components/PageButton.tsx
+++ src/components/PageButton.tsx
@@ -1,9 +1,11 @@
+import "../styles/PageButton.css"
+
 const PageButton = () => {
   return (
-    <>
+    <div className="page-button">
       <button>前へ</button>
       <button>次へ</button>
-    </>
+    </div>
   );
 };
 export default PageButton;
